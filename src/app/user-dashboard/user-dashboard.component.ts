8cda6cf46d5214a6047eb3bf6cf9cbf02eea1d18
Rayon Morgan <rayonaray@hotmail.com>
2022-04-15 22:56:52 -0500

    skeleton - side bar draft implemented with angular dynamic class feature and bootstrap columns

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  navStatusOn : boolean;
+  navStatusOff : boolean;
+
+  constructor() {
+    this.navStatusOn = false;
+    this.navStatusOff = true;
+   }
 
   ngOnInit(): void {
   }
+
+  // open and hide side nav bar
+  openSideNav() : void{
+     if(this.navStatusOn)
+     {
+       this.navStatusOff = true;
+       this.navStatusOn = false;
+     }
+     else
+     {
+      this.navStatusOff = false;
+      this.navStatusOn = true;
+     }
+  }
+  
 
 }
